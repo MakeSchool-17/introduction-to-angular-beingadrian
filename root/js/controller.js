3eec2367c666c3e0d7277910d334831d3545db7d
Adrian Wisaksana <dev@beingadrian.com>
2016-02-18 00:31:29 -0800

    Complete v.0.1 Beta
    Progress:
    - Implement initial UI
    - Implement search and simple commands
    - Connect with Firebase

--- root/js/controller.js
+++ root/js/controller.js
@@ -2,9 +2,10 @@
 
 angular
   .module('presenceApp.controller', ['presenceApp.services'])
-  .controller("MainCtrl", ['$scope', '$rootScope', 'Users', function($scope, $rootScope, Users) {
+  .controller("MainCtrl", ['$scope', 'Users', function($scope, Users) {
 
     $scope.users = Users.all();
+    $scope.results = Users.all();
     $scope.usersIn = Users.allIn();
     $scope.usersOut = Users.allOut();
 
@@ -12,6 +13,10 @@ angular
 
     $scope.summon = function() {
       console.log("Summoning!");
+    };
+
+    $scope.loginLogout = function() {
+      console.log("Log out...");
     };
 
     // MARK: - Search bar
@@ -22,17 +27,19 @@ angular
       var searchText = $scope.searchText.toLowerCase();
 
       if (searchText == "/in") {
-        $scope.users = Users.allIn();
+        $scope.results = Users.allIn();
       } else if (searchText == "/out") {
-        $scope.users = Users.allOut();
-      } else {
-        $scope.users = Users.filterByName(searchText);
+        $scope.results = Users.allOut();
+      } else if (searchText != "/") {
+        $scope.results = Users.filterByName(searchText);
       }
-      var userCount = $scope.users.length;
+      var userCount = $scope.results.length;
       if ($scope.searchText.length > 0) {
-        $scope.resultsString = $scope.users.length + " mactching results.";
+        if ($scope.results.length > 0) {
+          $scope.resultsString = $scope.results.length + " mactching results.";
+        }
       } else {
-        $scope.resultsString = $scope.usersIn.length + " users in. " + $scope.usersOut.length + " users out.";
+        $scope.resultsString = $scope.usersIn.length + " users in. " + $scope.usersOut.length + " users out. " + $scope.users.length + " users total."
       }
     };
 
--- root/js/resources.js
+++ root/js/resources.js
@@ -1,75 +1,27 @@
 // Resources.js
 
 angular
-  .module('presenceApp.services', [])
-  .factory('Users', function() {
+.module('presenceApp.services', ['firebase'])
+  .factory('Users', ['$firebaseArray', function($firebaseArray) {
 
-    var users = [
-      {
-        name: 'Pikachu',
-        status: 'in',
-        photoUrl: 'http://vignette3.wikia.nocookie.net/pokemon/images/1/16/025Pikachu_OS_anime_10.png/revision/20150102074354'
-      },
-      {
-        name: 'Caterpie',
-        status: 'in',
-        photoUrl: 'http://vignette3.wikia.nocookie.net/pokemon/images/8/89/010Caterpie_Dream.png/revision/latest?cb=20140911042209'
-      },
-      {
-        name: 'Jigglypuff',
-        status: 'out',
-        photoUrl: 'http://www.nintenderos.com/wp-content/uploads/2015/06/pc-jigglypuff.jpg'
-      },
-      {
-        name: 'Gengar',
-        status: 'out',
-        photoUrl: 'http://orig11.deviantart.net/3475/f/2012/015/0/f/gengar_the_shadow_pokemon_by_gengargrin-d4mf2te.jpg'
-      },
-      {
-        name: 'Snorlax',
-        status: 'out',
-        photoUrl: 'http://vignette3.wikia.nocookie.net/pokemon/images/9/9f/143Snorlax_OS_anime.png/revision/latest?cb=20140924022259'
-      },
-      {
-        name: 'Mew',
-        status: 'in',
-        photoUrl: 'http://img.pokemondb.net/artwork/mew.jpg'
-      },
-      {
-        name: 'Mewtwo',
-        status: 'out',
-        photoUrl: 'http://cdn.bulbagarden.net/upload/thumb/7/78/150Mewtwo.png/250px-150Mewtwo.png'
-      },
-      {
-        name: 'Magikarp',
-        status: 'in',
-        photoUrl: 'http://cdn.bulbagarden.net/upload/thumb/0/02/129Magikarp.png/250px-129Magikarp.png'
-      },
-      {
-        name: 'Dragonite',
-        status: 'out',
-        photoUrl: 'http://vignette3.wikia.nocookie.net/pokemon/images/0/07/149Dragonite_AG_anime.png/revision/latest?cb=20140126094014'
-      },
-      {
-        name: 'Mudkip',
-        status: 'in',
-        photoUrl: 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png'
-      }
-    ];
+    var ref = new Firebase('https://mspresence.firebaseio.com/users');
+    var users = $firebaseArray(ref);
+
+    var refIn = ref.orderByChild('status').equalTo('in');
+    var usersIn = $firebaseArray(refIn);
+
+    var refOut = ref.orderByChild('status').equalTo('out');
+    var usersOut = $firebaseArray(refOut);
 
     return {
       all: function() {
         return users;
       },
       allIn: function() {
-        return users.filter(function(user) {
-          return user.status == 'in';
-        });
+        return usersIn;
       },
-      allOut: function(){
-        return users.filter(function(user) {
-          return user.status == 'out';
-        });
+      allOut: function() {
+        return usersOut;
       },
       filterByName: function(queryString) {
         return users.filter(function(user) {
@@ -80,4 +32,4 @@ angular
       }
     };
 
-  });
+  }]);
